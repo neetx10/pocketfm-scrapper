3963a5100a72d262f0068336f19a48b10da6476b
Navdeep Soni <neet@live.in>
2023-10-15 03:09:37 +0530

    ui added

--- src/main/kotlin/com/example/pocketfmscrapping/controller/PocketFMUIController.kt
+++ src/main/kotlin/com/example/pocketfmscrapping/controller/PocketFMUIController.kt
@@ -1,26 +1,28 @@
 package com.example.pocketfmscrapping.controller
 
-import com.example.pocketfmscrapping.dto.Show
 import com.example.pocketfmscrapping.service.PocketFMDataService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
 
-@Controller
+@RestController
+@RequestMapping("/")
 class PocketFMUIController(val pocketFMDataService: PocketFMDataService) {
 
-    @RequestMapping("/", method = [RequestMethod.GET])
+    @RequestMapping(value = ["/","/shows"],method = [RequestMethod.GET])
     fun showList() : ModelAndView {
-        val mav = ModelAndView("shows");
+        val mav = ModelAndView("showList");
         mav.addObject("shows", pocketFMDataService.fetchAllShows())
         return mav
     }
 
-    @RequestMapping("/{showId}", method = [RequestMethod.GET])
-    fun episodeList(@PathVariable showId: String) : ModelAndView {
-        val mav = ModelAndView("episodes");
+    @RequestMapping("/episodes")
+    fun episodeList(@RequestParam showId: String) : ModelAndView {
+        val mav = ModelAndView("episodeList");
         mav.addObject("episodes", pocketFMDataService.fetchAllEpisodeByShow(showId))
         return mav
     }
